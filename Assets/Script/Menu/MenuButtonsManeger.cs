ab184a675c68773fe9783ef4797a986f0e47ed0a
Trifolio001 <Schitobengochea@gmail.com>
2024-06-26 03:34:57 -0300

    Aula 3- manutencao

--- Assets/Script/Menu/MenuButtonsManeger.cs
+++ Assets/Script/Menu/MenuButtonsManeger.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class MenuButtonsManeger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = 0.2f;
+    public float delay = 0.05f;
+    public  Ease ease = Ease.OutBack;
+
+    public void OnEnable()
     {
-        
+        HideAllButtons();
+        showButtons();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HideAllButtons()
     {
-        
+        foreach (var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+
+    private void showButtons()
+    {
+        Debug.Log("valor " + buttons.Count);
+        for(int i = 0; i < buttons.Count; i++)
+        {
+            Debug.Log("contando = " + i);
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i * delay).SetEase(ease);
+        }
     }
 }
